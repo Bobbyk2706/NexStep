8ee1a9e658b4debf3682796d27fce03eb852a3f4
Bobby <hiranyavenkatareddyk@gmail.com>
2026-08-26 14:52:52 +0530

    Add exam discovery service

--- backend/app/services/exam_discovery_service.py
+++ backend/app/services/exam_discovery_service.py
@@ -1,13 +1,5 @@
 import requests
-
-
 def fetch_website(url):
-
-    response = requests.get(
-        url,
-        timeout=20
-    )
-
+    response=requests.get(url,timeout=20)
     response.raise_for_status()
-
     return response.text
